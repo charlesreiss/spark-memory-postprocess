c3d495503a3a685e626e7c1d4457bc2a469ade64
Charles Reiss <woggle@apache.org>
2015-05-21 01:32:45 -0700

    Untested json read/write command support.

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "spark-memanalysis"
 
-version := "0.1"
+version := "0.2-SNAPSHOT"
 
 organization := "edu.berkeley.cs.amplab"
 
@@ -9,7 +9,8 @@ scalaVersion := "2.10.4"
 mainClass := Some("edu.berkeley.cs.amplab.sparkmem.ParseLogs")
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" % "spark-core_2.10" % "1.4.0-memanalysis-SNAPSHOT"
+  "org.apache.spark" % "spark-core_2.10" % "1.4.0-memanalysis-SNAPSHOT",
+  "org.json4s" %% "json4s-jackson" % "3.2.11"
 )
 
 {
--- src/main/scala/edu/berkeley/cs/amplab/sparkmem/Main.scala
+++ src/main/scala/edu/berkeley/cs/amplab/sparkmem/Main.scala
@@ -14,13 +14,7 @@ import org.apache.log4j.BasicConfigurator
 import org.apache.log4j.{Logger => L4JLogger, Level => L4JLevel}
 
 object Main {
-  def main(rawArgs: Array[String]) {
-    val conf = new SparkConf
-    val args = new Arguments(conf, rawArgs)
-    BasicConfigurator.configure()
-    if (args.debug) {
-      L4JLogger.getRootLogger.setLevel(L4JLevel.DEBUG)
-    } 
+  private def usageInfoFromEventLog(conf: SparkConf, args: Arguments): UsageInfo = {
     val fs = FileSystem.get(new Path(args.logDir).toUri, SparkHadoopUtil.get.newConfiguration(conf))
     val blockAccessListener = new BlockAccessListener
     blockAccessListener.skipExtraStacks = args.skipStacks || args.skipStacksExceptRDD
@@ -42,10 +36,41 @@ object Main {
     }
 
     Option(blockAccessListener.recordLogFile).foreach(_.close)
+
+    blockAccessListener.usageInfo
+  }
+
+  def usageInfoFromJson(args: Arguments): UsageInfo = {
+    import org.json4s._
+    import org.json4s.JsonDSL._
+    import org.json4s.jackson.JsonMethods._
+    import scala.io.Source
+   
+    UsageInfo.fromJson(parse(Source.fromFile(args.jsonFile).toString))
+  }
+
+  def main(rawArgs: Array[String]) {
+    val conf = new SparkConf
+    val args = new Arguments(conf, rawArgs)
+    BasicConfigurator.configure()
+    if (args.debug) {
+      L4JLogger.getRootLogger.setLevel(L4JLevel.DEBUG)
+    }
+    val usageInfo =
+      if (args.logDir != null)
+        usageInfoFromEventLog(conf, args)
+      else
+        usageInfoFromJson(args)
+
+    if (args.jsonFile != null && args.logDir != null) {
+      val writer = new PrintWriter(new File(args.jsonFile))
+      writer.write(usageInfo.toJsonString)
+      writer.close()
+    }
+
     if (args.makeConfig) {
       assert(!args.machineReadable)
       assert(args.targetWorkers > 0 || args.targetMemoryPerWorker > 0)
-      val usageInfo = blockAccessListener.usageInfo
       val config = if (args.targetWorkers > 0) {
           ProposedConfig.forWorkerCount(usageInfo, args.targetCoresPerWorker, args.targetWorkers)
         } else {
@@ -54,7 +79,6 @@ object Main {
         }
       println(config.configFile)
     } else if (args.machineReadable) {
-      val usageInfo = blockAccessListener.usageInfo
       println(usageInfo.csvHeader)
       for (i <- 1 to 16) {
         println(usageInfo.csvLine(i))
